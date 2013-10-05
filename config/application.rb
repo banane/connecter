316cc196ce465659b172c5ecb86e504e9d468641
Anna Billstrom <banane@gmail.com>
2013-10-04 23:16:45 -0700

    updated manifests

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ end
 module Women2
   class Application < Rails::Application
 
-    config.assets.precompile += %w( api/v1/people.css api/scaffold.css )
+    config.assets.precompile += %w( api/v1/people.css scaffold.css )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
