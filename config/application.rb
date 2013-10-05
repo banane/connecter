1159c3f67ce41c9d3b15227b0d24a6fa7556f409
Anna Billstrom <banane@gmail.com>
2013-10-04 23:06:10 -0700

    updated styles

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ end
 
 module Women2
   class Application < Rails::Application
+
+    config.assets.precompile += %w( api/v1/people.css api/scaffold.css )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
