642c0f87a24219d86a3b9c26c04c4cdfda738288
Anna Billstrom <banane@gmail.com>
2013-10-26 14:18:09 -0700

    added index and follower refresh page fix

--- app/controllers/api/v1/followed_people_controller.rb
+++ app/controllers/api/v1/followed_people_controller.rb
@@ -6,7 +6,13 @@ class Api::V1::FollowedPeopleController < ApplicationController
     :followed_person_id => params[:person_id])
     if @followship.save
       flash[:notice] = "Followed."
-      redirect_to api_v1_people_path(:auth_token=>@current_user.authentication_token)
+      if params[:from_view].eql?("following")
+        redirect_to api_v1_followed_people_path(:auth_token=>@current_user.authentication_token)
+      elsif params[:from_view].eql?("followers")
+        redirect_to follows_me_api_v1_followed_people_path(:auth_token=>@current_user.authentication_token)
+      else
+        redirect_to api_v1_people_path(:auth_token=>@current_user.authentication_token)
+      end
     else
       flash[:error] = "Unable to follow."
       redirect_to api_v1_people_path(:auth_token=>@current_user.authentication_token)
@@ -17,12 +23,20 @@ class Api::V1::FollowedPeopleController < ApplicationController
     @followship = @current_user.followed_people.find(params[:id])
     @followship.destroy
     flash[:notice] = "No longer following."
-    redirect_to api_v1_people_path(:auth_token=>@current_user.authentication_token)
+#    debugger
+    if params[:from_view].eql?("following")
+      redirect_to api_v1_followed_people_path(:auth_token=>@current_user.authentication_token)
+    elsif params[:from_view].eql?("followers")
+      redirect_to follows_me_api_v1_followed_people_path(:auth_token=>@current_user.authentication_token)
+    else
+      redirect_to api_v1_people_path(:auth_token=>@current_user.authentication_token)
+    end
   end
 
   def index
     @page = :following
-    @follows = FollowedPerson.where(:person_id => @current_user.id).includes(:person)
+    @follows = FollowedPerson.includes(:person).where('people.attending=1 and followed_people.person_id = ?',
+    @current_user.id).uniq
   end
 
   def follows_me
